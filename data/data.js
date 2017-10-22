6356a3eb28c84032160ad4d3ccd4ebae1316ca25
yohmer <yoh_m@yahoo.fr>
2017-10-22 02:09:53 -0500

    add articles in data file

--- data/data.js
+++ data/data.js
@@ -4,15 +4,93 @@ module.exports = {
       title: 'Apéritifs',
       articles: [
         {
-          title: 'martini',
+          nbr: '100',
+          title: 'martini rouge',
           price: '2.25'
         },
         {
+          nbr: '101',
           title:'porto',
+          price:'2.30'
+        },
+        {
+          nbr: '120',
+          title:'pastis',
+          price:'2.30'
+        },
+        {
+          nbr: '203',
+          title:'cuba libre',
+          price:'2.30'
+        },
+        {
+          nbr: '105',
+          title:'martini blanc',
+          price:'2.30'
+        },
+        {
+          nbr: '731',
+          title:'whisky',
           price:'2.30'
         }]
     },
-    {title: 'Vins'},
-    {title: 'Sans Alcool'}
+    {
+      title: 'Vins',
+      articles: [
+        {
+          nbr: '200',
+          title: 'verre de vin rouge',
+          price: '2.25'
+        },
+        {
+          nbr: '201',
+          title:'verre de vin blanc',
+          price:'2.30'
+        },
+        {
+          nbr: '220',
+          title:'verre de rosé',
+          price:'2.30'
+        },
+        {
+          nbr: '303',
+          title:'côte du Rhöne (bouteille)',
+          price:'2.30'
+        },
+        {
+          nbr: '205',
+          title:'champagne',
+          price:'2.30'
+        }]
+      },
+    {
+      title: 'Sans Alcool',
+      articles: [
+        {
+          nbr: '300',
+          title: 'thé vert',
+          price: '2.25'
+        },
+        {
+          nbr: '301',
+          title:'thé glacé',
+          price:'2.30'
+        },
+        {
+          nbr: '320',
+          title:'jus de tomate',
+          price:'2.30'
+        },
+        {
+          nbr: '403',
+          title:'jus de melon',
+          price:'2.30'
+        },
+        {
+          nbr: '305',
+          title:'limonade',
+          price:'2.30'
+        }]
+    }
   ]
 };
