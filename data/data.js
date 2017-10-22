ded25993a384e75c610be54977555528e1ba9a94
yohmer <yoh_m@yahoo.fr>
2017-10-21 19:00:30 -0500

    handle data file

---
+++ data/data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  boissons:[
+    {
+      title: 'Apéritifs',
+      articles: [{title: 'martini'}, {title:'porto'}]
+    },
+    {title: 'Vins'},
+    {title: 'Sans Alcool'}
+  ]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,10 @@
+var data = require('../data/data');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',
-  {
-    boissons:[
-      {
-        title: 'Apéritifs',
-        articles: [{title: 'martini'}, {title:'porto'}]
-      },
-      {title: 'Vins'},
-      {title: 'Sans Alcool'}
-    ]
-  });
+  res.render('index',data);
 });
 
 module.exports = router;
