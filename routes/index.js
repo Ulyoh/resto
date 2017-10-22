726be180ddb8495c04846339b686effea7c94550
yohmer <yoh_m@yahoo.fr>
2017-10-22 11:21:46 -0500

    add preparation for i18n support

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-var data = require('../data/data');
+var articles = require('../data/articles');
+var i18n = require('../data/i18n');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', data.boissons);
+  var data = { articles: articles.boissons, i18n: i18n };
+  res.render('index', data);
 });
 
 module.exports = router;
