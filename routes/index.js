30b5fdccbdd49a48c3f6c920b72d3120334247d7
yohmer <yoh_m@yahoo.fr>
2017-10-24 21:56:16 -0500

    regroup all i18n information in one file
    no management of differents files at this point.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-var articles = require('../data/articles');
 var i18n = require('../data/i18n');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = { articles: articles[0].articles_groups, text: articles[0], i18n: i18n };
+  var data = { articles_related: i18n.articles_related[0], alerts_msg: i18n.alerts_msg };
   res.render('index', data);
 });
 
